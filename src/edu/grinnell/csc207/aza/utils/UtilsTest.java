2d13929ae6d0a2372026d8448bbc46cff948c6a5
wolterzo <wolterzo@grinnell.edu>
2014-09-20 12:53:51 -0500

    add tests for exponentiation

--- src/edu/grinnell/csc207/aza/utils/UtilsTest.java
+++ src/edu/grinnell/csc207/aza/utils/UtilsTest.java
@@ -13,4 +13,12 @@ public class UtilsTest
     fail("Not yet implemented");
   }
 
+  @Test
+  public void testExpt()
+  {
+    assertEquals("zero base", 0, Utils.expt(0, 6));
+    assertEquals("zero power", 1, Utils.expt(5, 0));
+    assertEquals("even power", 1024, Utils.expt(2, 10));
+    assertEquals("odd power", 512, Utils.expt(2, 9));
+  }
 }
